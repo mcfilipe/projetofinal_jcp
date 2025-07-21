6f9484ab195cb9e1f673da240898b57c2ceb8e91
mcfilipe <mcfilipe@gmail.com>
2025-07-21 16:00:44 +0100

    Criado o metodo para ler ficheiros TXT

--- minhas_libs.py
+++ minhas_libs.py
@@ -1,3 +1,14 @@
 """
 Aqui iremos colocar as bibliotecas que iremos utilizar no projeto.
 """
+
+def ler_ficheiro(nome_do_ficheiro):
+    """
+    método par abrir um ficheiro em modo leitura e imprimir o conteúdo do ficheiro
+    para: nome_do_ficheiro.txt
+    """
+    meu_ficheiro = open(nome_do_ficheiro, "r", encoding="utf-8")
+    for line in meu_ficheiro:
+        print(line.strip()) 
+        meu_ficheiro.close()
+    meu_ficheiro.close()
